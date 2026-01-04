0ce4784ea788cd9b265db628a53114372ae6635e
Achala C <chandruachala9@gmail.com>
2026-01-04 18:01:06 +0530

    Create Lab10.java

---
+++ JAVA/Lab10.java
@@ -0,0 +1,13 @@
+/*Develop a JAVA program to create a package named mypack and import & implement it in a 
+suitable class.
+✅ Step 1: Create the Package (mypack)
+File: mypack/Message.java*/
+package mypack; // Declare package name
+// Class inside the package 
+public class Message { 
+public void display() {
+System.out.println("Hello from mypack!");
+}
+}
+
+/*
